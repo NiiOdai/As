31de2c55ba7e89b1e6190a1c7d555e4f36884097
NiiOdai <mkookley-mensah@st.ug.edu.gh>
2021-10-14 09:17:00 +0000

    bvcx

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
+  const MainNavigator =createStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <NavigationContainer>art
+       <MainNavigator>
+         <MainNavigator.Screen name ="Login" component={Login}/>
+         <MainNavigator.Screen name ='Cart' component={Cart}/>
+         <MainNavigator.Screen name ='Home' component={Login}/>
+       </MainNavigator>
+     </NavigationContainer>
+
+      // <StatusBar style="auto" />
     </View>
   );
 }
