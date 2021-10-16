e1c793735ef7966e18965ec590c6a506b5a20336
NiiOdai <mkookley-mensah@st.ug.edu.gh>
2021-10-16 01:40:22 +0000

    change

--- App.js
+++ App.js
@@ -10,13 +10,8 @@ import Cart from './screens/Cart';
 export default function App() {
   const MainNavigator = createNativeStackNavigator();
   return (
-<<<<<<< HEAD
         <NavigationContainer>
       <MainNavigator.Navigator screenOptions={{headerShown:false}} initialRouteName = 'Login'>
-=======
-    <NavigationContainer>
-      <MainNavigator.Navigator initialRouteName = 'Home'>
->>>>>>> a61a53602a66d2edbbfb07e9e04a9cac582332f6
         <MainNavigator.Screen name = 'Home' component={Home}/>
         <MainNavigator.Screen name = 'Login' component={Login}/>
         <MainNavigator.Screen name = 'Cart' component={Cart}/>        
@@ -26,4 +21,6 @@ export default function App() {
   );
     
   }
+   
+  
   
