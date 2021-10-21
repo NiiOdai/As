99a1f48950ea7496205c044502a6f0c23d6fa11a
NiiOdai <83757220+NiiOdai@users.noreply.github.com>
2021-10-21 07:41:29 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { NavigationContainer } from '@react-navigation/native';
-import Home from './screens/Home';
-import Login from './screens/Login';
-import Cart from './screens/Cart';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
 
 export default function App() {
   const MainNavigator = createNativeStackNavigator();
-  return (          
-        
-        <NavigationContainer>
-      <MainNavigator.Navigator screenOptions={{headerShown:false}} initialRouteName = 'Cart'>
-        <MainNavigator.Screen name = 'Home' component={Home}/>
-        <MainNavigator.Screen name = 'Login' component={Login}/>
-        <MainNavigator.Screen name = 'Cart' component={Cart}/>        
-      </MainNavigator.Navigator>
-    </NavigationContainer>    
-  
+
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationContainer>
+        <MainNavigator.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Home"
+        >
+          <MainNavigator.Screen name="Login" component={Login} />
+          <MainNavigator.Screen name="Home" component={Home} />
+        </MainNavigator.Navigator>
+      </NavigationContainer>
+    </View>
   );
-    
-  }
-   
-  
-  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
